05268718b7004865354a0c1d00db850e3e23b44f
einaros <einaros@gmail.com>
2012-06-19 18:44:01 +0200

    enable multiple runs of a benchmark

---
+++ compare.js
@@ -0,0 +1,22 @@
+var fs = require('fs')
+  , util = require('util');
+require('./lib/shared');
+
+if (process.argv.length < 4) {
+  console.error('usage: compare <result file 1> <result file 2>');
+  process.exit(-1);
+}
+var fileA = process.argv[2];
+var resA = JSON.parse(fs.readFileSync(fileA));
+var fileB = process.argv[3];
+var resB = JSON.parse(fs.readFileSync(fileB));
+
+var keys = Object.keys(resA);
+for (var i = 0, l = keys.length; i < l; ++i) {
+  var key = keys[i];
+  var rA = resA[key];
+  var rB = resB[key];
+  var aIsFaster = rA.speed > rB.speed;
+  var desc = util.format('%d roundtrips of %s %s data: %s %d%% faster', rA.roundtrips, humanSize(rA.size), rA.binary ? 'binary' : 'text', aIsFaster ? fileA : fileB, Math.round((aIsFaster ? (rA.speed / rB.speed) : (rB.speed / rA.speed)) * 100) - 100);
+  console.log(desc);
+}
--- ws.js
+++ ws.js
@@ -7,68 +7,109 @@ var cluster = require('cluster')
 require('tinycolor');
 require('./lib/shared');
 
+var silent = process.argv[2] == '--silent';
+var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
 if (cluster.isMaster) {
-  var wss = new WebSocketServer({port: 8181}, function() {
-    cluster.fork();
-  });
-  wss.on('connection', function(ws) {
-    ws.on('message', function(data, flags) {
-      ws.send(data, {binary: flags&&flags.binary});
+  var repetitions = config.repetitions;
+
+  var benchmarkRuns = [];
+  var wss;
+  function repeat() {
+    if (repetitions == 0) return false;
+    repetitions -= 1;
+    if (wss) {
+      wss.close();
+      wss = null;
+    }
+    wss = new WebSocketServer({port: 8181}, function() {
+      var child = cluster.fork();
+      child.on('message', function(message) {
+        if (message.cmd == 'complete') {
+          benchmarkRuns.push(message.data);
+          if(!repeat()) {
+            if (!silent) console.log('Benchmark complete');
+            var totalResults = {};
+            for (var i = 0; i < benchmarkRuns.length; ++i) {
+              var benchmarkRun = benchmarkRuns[i];
+              for (var k = 0; k < benchmarkRun.length; ++k) {
+                var result = benchmarkRun[k]; 
+                if (typeof totalResults[k] == 'undefined') totalResults[k] = result;
+                else totalResults[k].speed += result.speed;
+              }
+            }
+            var keys = Object.keys(totalResults);
+            for (var i = 0; i < keys.length; ++i) {
+              totalResults[keys[i]].speed = totalResults[keys[i]].speed / benchmarkRuns.length;
+            }
+            console.log(JSON.stringify(totalResults));
+            process.exit();
+          }
+        }
+      });
     });
-    ws.on('close', function() {});
-  });
-  cluster.on('exit', function(worker) {
-    console.log('Client closed');
-    process.exit();
-  });
+    wss.on('connection', function(ws) {
+      ws.on('message', function(data, flags) {
+        ws.send(data, {binary: flags&&flags.binary});
+      });
+      ws.on('close', function() {});
+    });
+    return true;
+  }
+  
+  repeat();
 }
 else {
   var cursor = ansi(process.stdout);
-  var cases = JSON.parse(fs.readFileSync('config.json', 'utf8')).cases;
-  var largest = cases[0][2];
+  var cases = config.cases;
+  var results = [];
+  var largest = cases[0].size;
   for (var i = 0, l = cases.length; i < l; ++i) {
-    if (cases[i][2] > largest) largest = cases[i][2];
+    if (cases[i].size > largest) largest = cases[i].size;
   }
 
-  console.log('Generating %s of test data ...', humanSize(largest));
+  if (!silent) console.log('Generating %s of test data ...', humanSize(largest));
   var randomBytes = generateRandomData(largest);
 
-  function roundtrip(useBinary, roundtrips, size, cb) {
-    var data = randomBytes.slice(0, size);
-    if (useBinary == false) data = data.toString('utf8');
-    var prefix = util.format('Running %d roundtrips of %s %s data', roundtrips, humanSize(size), useBinary ? 'binary' : 'text');
-    console.log(prefix);
-    var client = new WebSocket('ws://localhost:' + '8181');
+  function roundtrip(benchmarkCase, cb) {
+    var data = randomBytes.slice(0, benchmarkCase.size);
+    if (benchmarkCase.binary == false) data = data.toString('utf8');
+    var prefix = util.format('Running %d roundtrips of %s %s data', benchmarkCase.roundtrips, humanSize(benchmarkCase.size), benchmarkCase.binary ? 'binary' : 'text');
+    if (!silent) console.log(prefix);
+    var ws = new WebSocket('ws://localhost:' + '8181');
     var dt;
     var counterId;
     var roundtrip = 0;
     function send() {
-      client.send(data, {binary: useBinary});
+      ws.send(data, {binary: benchmarkCase.binary});
     }
-    client.on('error', function(e) {
+    ws.on('error', function(e) {
       console.error(e);
       process.exit();
     });
-    client.on('open', function() {
+    ws.on('open', function() {
       counterId = setInterval(function() {
-        cursor.up();
-        console.log('%s:\t%ds ...'
+        if (!silent) cursor.up();
+        if (!silent) console.log('%s:\t%ds ...'
           , prefix
           , ~~((Date.now() - dt) / 1000));
       }, 1000);
       dt = Date.now();
       send();
     });
-    client.on('message', function(data, flags) {
-      if (++roundtrip == roundtrips) {
+    ws.on('message', function(data, flags) {
+      if (++roundtrip == benchmarkCase.roundtrips) {
         var elapsed = Date.now() - dt;
         clearInterval(counterId);
-        cursor.up();
-        console.log('%s:\t%ss\t%s'
-          , useBinary ? prefix.green : prefix.cyan
+        if (!silent) cursor.up();
+        benchmarkCase.speed = (benchmarkCase.size * benchmarkCase.roundtrips) / elapsed * 1000;
+        if (!silent) console.log('%s:\t%ss\t%s'
+          , benchmarkCase.binary ? prefix.green : prefix.cyan
           , roundPrec(elapsed / 1000, 1).toString().green.bold
-          , (humanSize((size * roundtrips) / elapsed * 1000) + '/s').blue.bold);
-        client.close();
+          , (humanSize(benchmarkCase.speed) + '/s').blue.bold);
+
+        results.push(benchmarkCase);
+        ws.close();
         cb();
         return;
       }
@@ -76,10 +117,13 @@ else {
     });
   }
 
-  (function run() {
-    if (cases.length == 0) process.exit();
-    var config = cases.shift();
-    config.push(run);
-    roundtrip.apply(null, config);
-  })();
+  function runBenchmark() {
+    if (cases.length == 0) {
+      process.send({cmd: 'complete', data: results});
+      process.exit();
+    }
+    roundtrip(cases.shift(), runBenchmark);
+  }
+  runBenchmark();
 }
+
